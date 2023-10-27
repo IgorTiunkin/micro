20357c850e20cef747bf0469da4c76219a92209a
Igor Tiunkin <fantomenergy@gmail.com>
2023-10-27 22:38:30 +0300

    Added and integrated Discovery Server

--- order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -40,8 +40,8 @@ public class OrderService {
                 .stream().map(OrderLineItems::getSkuCode)
                 .collect(Collectors.toList());
         //Use inventory service to check availability
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
